220ca72509201446587c8e4ebbdf8863a264507e
Adi Eyal <adi@clearforest.io>
2026-03-30 01:48:08 +0300

    Bump version to 0.2.0

--- src/metricengine/__init__.py
+++ src/metricengine/__init__.py
@@ -17,7 +17,7 @@ Key Features:
 from importlib.metadata import PackageNotFoundError, version
 from typing import Optional
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 try:
     _installed_version = version("metric-engine")
